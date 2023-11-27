71cc9134a655b0ff006d5ec0b74a7094ddb573ad
Robert Gleason <l2ob122@gmail.com>
2023-11-26 20:05:00 -0800

    added a try catch for error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,30 +19,41 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData,
 ) {
-  // check the users inputs and make sure they're valid
-  const title = formData.get('title') as string;
-  const code = formData.get('code') as string;
+  try {
+    // check the users inputs and make sure they're valid
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
 
-  // form validation checks
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Title must be at least 3 characters long',
-    };
-  }
-  if (typeof code !== 'string' || code.length < 10) {
-    return {
-      message: 'Code must be at least 10 characters long',
-    };
-  }
+    // form validation checks
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be at least 3 characters long',
+      };
+    }
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be at least 10 characters long',
+      };
+    }
 
-  // create a new record in the db
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-  console.log(snippet);
+    // create a new record in the db
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: 'Something went wrong',
+      };
+    }
+  }
 
   // redirect the user back to the root route
   redirect('/');
