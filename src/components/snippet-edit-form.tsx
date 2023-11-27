36136d166c0f962010c4facbdb1ec5b76598774a
Robert Gleason <l2ob122@gmail.com>
2023-11-26 18:44:15 -0800

    added the on change to the Editor

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,12 +2,17 @@
 
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
   return (
     <div>
       <Editor
@@ -16,6 +21,7 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
         language="javascript"
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </div>
   );
