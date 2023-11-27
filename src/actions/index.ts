3235f52674dcdf404a67915b8331ae4864f16b60
Robert Gleason <l2ob122@gmail.com>
2023-11-26 21:04:12 -0800

    added on demand caching to some of the server components

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 'use server';
 import { db } from '@/db';
 import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
@@ -12,6 +13,7 @@ export async function editSnippet(id: number, code: string) {
 
 export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
+  revalidatePath(`/`);
   redirect(`/`);
 }
 
@@ -54,7 +56,7 @@ export async function createSnippet(
       };
     }
   }
-
+  revalidatePath(`/`);
   // redirect the user back to the root route
   redirect('/');
 }
